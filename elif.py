7bd0c7629c2af9f356e10c755c1546b676e94199
Kusuma418 <indlakusuma630@gmail.com>
2024-09-09 22:59:48 +0530

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,10 @@
+#elif
+def pos_neg():
+    num=int(input())
+    if(num>0):
+        print("positive number")
+    elif(num<0):
+        print("negative number")
+    else:
+        print("neutral number")
+pos_neg()
---
+++ ifelse.py
@@ -0,0 +1,8 @@
+#if else
+def voting():
+    age=int(input())
+    if age>=18:
+        print("you are eligible for voting")
+    else:
+        print("you're not eligible for voting")
+voting()
