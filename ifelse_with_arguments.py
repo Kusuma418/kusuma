c3ac23f5ca6a302ab8c1a999ddbe43a0eaa248ff
Kusuma418 <indlakusuma630@gmail.com>
2024-09-09 23:15:41 +0530

    Add files via upload

---
+++ ifelse_with_arguments.py
@@ -0,0 +1,8 @@
+#if else with argumemts
+def voting(age):
+    if age>=18:
+        print("you are eligible for voting")
+    else:
+        print("you're not eligible for voting")
+#examples:
+voting(22)
