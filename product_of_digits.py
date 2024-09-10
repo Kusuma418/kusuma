12a586d8eefa9f6ed4def41e0a5d60e8a0f36ad0
Kusuma418 <indlakusuma630@gmail.com>
2024-09-10 17:44:58 +0530

    Update product_of_digits.py

--- product_of_digits.py
+++ product_of_digits.py
@@ -1,9 +1,9 @@
 #product of digits from a given number
 def prodofDigits(num):
-    prod=0
+    prod=1
     while(num!=0):
         last_digit=num%10 #extract last digit from given number
         prod*=last_digit
         num//=10  #remove last digit from given number
-    print(prod)  #return prodS
+    print(prod)  #return prod
 
